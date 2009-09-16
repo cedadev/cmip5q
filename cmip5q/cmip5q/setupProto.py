036555d00bc5dcd5e736158e18ddf11b75e0729b
bryan <bryan@4926d154-5f4e-0410-ae62-53d2c911a6ca>
2009-09-16 13:55:10 +0000

    Attempting to make it easier to work with the questionnaire in windows.
    git-svn-id: http://proj.badc.rl.ac.uk/svn/metafor/cmip5q/trunk@889 4926d154-5f4e-0410-ae62-53d2c911a6ca

---
+++ cmip5q/cmip5q/setupProto.py
@@ -0,0 +1,32 @@
+
+import os
+
+# get the cmip5 settings
+os.environ['DJANGO_SETTINGS_MODULE']='settings'
+
+from protoq.models import *
+from XMLinitialiseQ import initialise
+from XMLActivityReader import NumericalExperiment
+from NumericalModel import *
+from initialiseRefs import *
+from initialiseFiles import *
+
+# Initialise the Questionnaire
+initialise()
+
+# load cmip5 input files
+initialiseFiles()
+
+# load cmip5 input references
+initialiseRefs()
+
+# create experiments
+
+experimentDir = './data/experiments'
+for f in os.listdir(experimentDir):
+    if f.endswith('.xml'):
+	    x=NumericalExperiment(os.path.join(experimentDir, f)) 
+	    x.load()
+
+# initialise a model template
+initialiseModel()
--- cmip5q/cmip5q/setupProto.sh
+++ cmip5q/cmip5q/setupProto.sh
@@ -1,44 +1,13 @@
+# This shell script executes the necessary setup for the questionnaire
+
 # comment out the following line for deployment
 source ~/meta4q/bin/activate
 
-# This only works if you use sqlite of course ...
+# only do this in testing
 rm -f sqlite.db
 
-# for now we don't have a superuser, which means no admin
-${PYTHON:-python} manage.py syncdb <<EOF
-no
-EOF
+# reset the model in sql storage
+${PYTHON:-python} manage.py syncdb --noinput
 
-# set up some things after a complete database rewrite
-${PYTHON:-python} manage.py shell << EOF
-
-from protoq.models import *
-from XMLinitialiseQ import initialise
-from XMLActivityReader import NumericalExperiment
-from NumericalModel import *
-from initialiseRefs import *
-from initialiseFiles import *
-
-# Initialise the Questionnaire
-initialise()
-
-# load cmip5 input files
-initialiseFiles()
-
-# load cmip5 input references
-initialiseRefs()
-
-# create experiments
-
-import os
-experimentDir = './data/experiments'
-for f in os.listdir(experimentDir):
-    if f.endswith('.xml'):
-	    x=NumericalExperiment(os.path.join(experimentDir, f)) 
-	    x.load()
-
-# initialise a model template
-initialiseModel()
-
-
-EOF
+# initialise questionnaire values
+${PYTHON:-python} setupProto.py
