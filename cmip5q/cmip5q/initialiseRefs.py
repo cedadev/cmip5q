c73730ba427c86fbb12a9e30a28e1313680db13e
gerry <gerry@4926d154-5f4e-0410-ae62-53d2c911a6ca>
2009-10-01 13:06:11 +0000

    Updated InitialiseRefs with code to read in csv reference file
    git-svn-id: http://proj.badc.rl.ac.uk/svn/metafor/cmip5q/trunk@950 4926d154-5f4e-0410-ae62-53d2c911a6ca

--- cmip5q/cmip5q/initialiseRefs.py
+++ cmip5q/cmip5q/initialiseRefs.py
@@ -1,5 +1,40 @@
 from protoq.models import *
+import csv
+import logging
+
+
 def initialiseRefs():
     ''' This routine initialises the database with some obvious files for boundary conditoins etc '''
-    pass
+    CSVinfo = csv.reader(open('Refs_CSV.csv'), delimiter=';', quotechar='|')
+    # this is the vocab that we always use for reference types:
+    v=Vocab.objects.get(name='Reference Types Vocab')
+    # loop over all rerences in spreadsheet
+    for row in CSVinfo:
+        print row
+        logging.debug(', '.join(row))
+        name,citation,link=tuple(row)
+        # find out what refrence type
+        myreftype='Online Refereed'
+        try:
+            refType=Value.objects.filter(vocab=v).get(value=myreftype)
+        except:
+            logging.info('Ignoring reference %s'%', '.join(row))
+            break  # leave the loop
+        # find the other things: name, citation, link    
+        r=Reference(name=name,
+                    citation=citation,
+                    link=link,
+                    refTypes=v,
+                    refType=refType)
+        try:
+            r.save()
+        except:
+            logging.info('Unable to save reference %s'%name)
+        # temporary 2 lines
+        def __unicode__(self):
+            return
     
+if __name__=="__main__":
+    
+     initialiseRefs()
+     pass
