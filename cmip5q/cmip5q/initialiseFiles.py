c052303adab9a5fef5e2e8c4108dce41d3fc999c
gerry <gerry@4926d154-5f4e-0410-ae62-53d2c911a6ca>
2009-10-30 14:06:15 +0000

    Code to upload 'files' into questionnaire
    git-svn-id: http://proj.badc.rl.ac.uk/svn/metafor/cmip5q/trunk@1050 4926d154-5f4e-0410-ae62-53d2c911a6ca

--- cmip5q/cmip5q/initialiseFiles.py
+++ cmip5q/cmip5q/initialiseFiles.py
@@ -1,6 +1,38 @@
 from protoq.models import *
+import csv
+import logging
+
 def initialiseFiles():
     ''' This routine initialises the database with some obvious files for boundary conditoins etc '''
-    pass
+    FilesCSVinfo = csv.reader(open('Files_CSV.csv'), delimiter=';', quotechar='|')
+    # this is the vocab that we always use for reference types:
+    v=Vocab.objects.get(name='FileFormats')
+    # loop over all rerences in spreadsheet
+    for row in FilesCSVinfo:
+        print row
+        logging.debug(', '.join(row))
+        # format is being read into the tuple only for convenience at the moment but is overwritten
+        name,link,format,description=tuple(row)
+        # find out what file type
+        filetype='Other'
+        try:
+            format=Value.objects.filter(vocab=v).get(value=filetype)
+        except:
+            logging.info('Ignoring file %s'%', '.join(row))
+            break  # leave the loop
+        # find the other things: name, description, link    
+        f=DataContainer(name=name,
+                    link=link,
+                    description=description,
+                    format=format)
+        try:
+            f.save()
+        except:
+            logging.info('Unable to save file %s'%name)
+        # temporary 2 lines
+        def __unicode__(self):
+            return
     
-   
+if __name__=="__main__":
+    
+    initialiseFiles()    
