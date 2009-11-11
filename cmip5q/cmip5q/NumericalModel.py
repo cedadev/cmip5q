70c4ee9a594b58e20f8cff4821bc859baa763669
rupert <rupert@4926d154-5f4e-0410-ae62-53d2c911a6ca>
2009-11-11 17:10:36 +0000

    Work towards output of all component information including coupling, for questionnaire to CIM1.3. Plus questionnaire specific additions where mapping has not been agreed.
    git-svn-id: http://proj.badc.rl.ac.uk/svn/metafor/cmip5q/trunk@1097 4926d154-5f4e-0410-ae62-53d2c911a6ca

--- cmip5q/cmip5q/NumericalModel.py
+++ cmip5q/cmip5q/NumericalModel.py
@@ -136,14 +136,131 @@ class NumericalModel:
         self.exportAddComponent(root,self.top,1,recurse)
         return root
 
+    def addResp(self,parent,respType,p):
+        if (p) :
+            resp=ET.SubElement(parent,"Q_responsibleParty",{'type':respType})
+            ET.SubElement(resp,"Q_name").text=p.name
+            ET.SubElement(resp,"Q_webpage").text=p.webpage
+            ET.SubElement(resp,"Q_abbrev").text=p.abbrev
+            ET.SubElement(resp,"Q_email").text=p.email
+            ET.SubElement(resp,"Q_address").text=p.address
+            ET.SubElement(resp,"Q_uri").text=p.uri
+        #resp=ET.SubElement(comp,'responsibleParty') #type gmd:xxxx
+        #ciresp=ET.SubElement(resp,self.GMD_NAMESPACE_BRACKETS+'CI_ResponsibleParty')
+        #http://www.isotc211.org/2005/gmd
+        #CI_ResponsibleParty referenced in citation.xsd
+        # <gmd:individualName>
+        #name=ET.SubElement(resp,'gmd:individualName')
+        #ET.SubElement(name,'gco:CharacterString').text=c.contact
+        #name=ET.SubElement(ciresp,self.GMD_NAMESPACE_BRACKETS+'individualName')
+        ###ET.SubElement(name,self.GCO_NAMESPACE_BRACKETS+'CharacterString').text=c.contact
+        # </gmd:individualName/>
+        # <gmd:organisationName/>
+        # <gmd:positionName/>
+        # <gmd:contactInfo>
+        #contact=ET.SubElement(ciresp,'gmd:contactInfo')
+        #contact=ET.SubElement(ciresp,self.GMD_NAMESPACE_BRACKETS+'contactInfo')
+        #cicontact=ET.SubElement(contact,self.GMD_NAMESPACE_BRACKETS+'CI_Contact')
+        #     <gmd:phone/>
+        #     <gmd:address>
+        #address=ET.SubElement(cicontact,'gmd:address')
+        #address=ET.SubElement(cicontact,self.GMD_NAMESPACE_BRACKETS+'address')
+        #ciaddress=ET.SubElement(address,self.GMD_NAMESPACE_BRACKETS+'CI_Address')
+        #         <gmd:deliveryPoint/>
+        #         <gmd:city/>
+        #         <gmd:administrativeArea/>
+        #         <gmd:postalCode/>
+        #         <gmd:country/>
+        #         <gmd:electronicMailAddress>
+        #email=ET.SubElement(ciaddress,'gmd:electronicMailAddress')
+        #ET.SubElement(email,'gco:CharacterString').text=c.email
+        #email=ET.SubElement(ciaddress,self.GMD_NAMESPACE_BRACKETS+'electronicMailAddress')
+        ###ET.SubElement(email,self.GCO_NAMESPACE_BRACKETS+'CharacterString').text=c.email
+        #         </gmd:electronicMailAddress>
+        #     </gmd:address>
+        #     <gmd:onlineResource/>
+        #     <gmd:hoursOfService/>
+        #     <gmd:contactInstructions/>
+        # </gmd:contactInfo>
+        # <gmd:roll/>
+        #role=ET.SubElement(ciresp,self.GMD_NAMESPACE_BRACKETS+'role')#empty
+
+    def getTimeStepParam(self,c):
+        # c/child/TimeSteppingFramework/TimeStep
+        for child in c.components.all():
+            if child.implemented:
+                pgset=ParamGroup.objects.filter(component=child)
+                for pg in pgset:
+                    if pg.name=='TimeSteppingFramework':
+                        constraintSet=ConstraintGroup.objects.filter(parentGroup=pg)
+                        for con in constraintSet:
+                            pset=NewParam.objects.filter(constraint=con)
+                            for p in pset:
+                                if p.name=='TimeStep':
+                                    return p
+        return 0
+
+
+    def composition(self,c,comp):
+        couplings=Coupling.objects.filter(component=c)
+        if len(couplings)>0:
+            composition=ET.SubElement(comp,'composition')
+            for coupling in couplings:
+                connection=ET.SubElement(composition,'coupling')
+                ET.SubElement(connection,'Q_sourceComponent').text=c.abbrev
+                ET.SubElement(connection,'Q_sourceComponent').text=coupling.component.abbrev
+                ET.SubElement(connection,'Q_inputType').text=coupling.targetInput.ctype.value
+                ET.SubElement(connection,'Q_inputAbbrev').text=coupling.targetInput.abbrev
+                ET.SubElement(connection,'Q_inputDescrip').text=coupling.targetInput.description
+                if coupling.targetInput.ctype.value=='BoundaryCondition':
+                    ET.SubElement(connection,'Q_type').text=coupling.ctype.value
+                    ET.SubElement(connection,'Q_freqUnits').text=coupling.FreqUnits.value
+                    ET.SubElement(connection,'Q_frequency').text=str(coupling.couplingFreq)
+                    ET.SubElement(connection,'Q_manipulation').text=coupling.manipulation
+                iClosures=InternalClosure.objects.filter(coupling=coupling)
+                if len(iClosures)>0:
+                    closures=ET.SubElement(connection,'Q_internalClosures')
+                    for iclos in iClosures:
+                        closure=ET.SubElement(closures,'Q_closure')
+                        ET.SubElement(closure,'Q_spatialRegridding').text=iclos.spatialRegridding.value
+                        ET.SubElement(closure,'Q_spatialType').text=iclos.spatialType.value
+                        ET.SubElement(closure,'Q_temporalRegridding').text=iclos.temporalRegridding.value
+                        ET.SubElement(closure,'Q_inputDescription').text=iclos.inputDescription
+                        ET.SubElement(closure,'Q_target').text=iclos.target.abbrev
+                eClosures=ExternalClosure.objects.filter(coupling=coupling)
+                if len(eClosures)>0:
+                    closures=ET.SubElement(connection,'Q_externalClosures')
+                    for eclos in eClosures:
+                        closure=ET.SubElement(closures,'Q_closure')
+                        ET.SubElement(closure,'Q_spatialRegridding').text=eclos.spatialRegridding.value
+                        ET.SubElement(closure,'Q_spatialType').text=eclos.spatialType.value
+                        ET.SubElement(closure,'Q_temporalRegridding').text=eclos.temporalRegridding.value
+                        ET.SubElement(closure,'Q_inputDescription').text=eclos.inputDescription
+                        ET.SubElement(closure,'Q_target').text=eclos.target.variable
+                #coupling=ET.SubElement(composition,'coupling')
+                #ET.SubElement(composition,'description').text='Composition information associated with component '+c.abbrev
+                '''connection'''
+                '''description'''
+                '''timeProfile'''
+                '''timeLag'''
+                '''spatialRegridding'''
+                '''timeTransformation'''
+                '''couplingSource'''
+                #source=ET.SubElement(coupling,'couplingSource',{'purpose':'xxx','fullySpecified':'????'})
+                #sourceRef=ET.SubElement(source,'reference',{'xlinkXXXhref':'AddLocationHere'})
+                '''couplingTarget'''
+                '''priming'''
+
     def exportAddComponent(self,root,c,nest,recurse=True):
 
       if c.implemented:
         if nest==1:
-          comp=ET.SubElement(root,'modelComponent',{'documentVersion': '-1'}) # TBD2
+          # TBD documentVersion
+          comp=ET.SubElement(root,'modelComponent',{'documentVersion': '-1', 'CIMVersion': '1.3'})
         else:
           comp=ET.SubElement(root,'modelComponent')
         '''composition'''
+        self.composition(c,comp)
         if recurse:
             '''childComponent'''
             for child in c.components.all():
@@ -163,28 +280,36 @@ class NumericalModel:
         componentProperties=ET.SubElement(comp,'componentProperties')
         pgset=ParamGroup.objects.filter(component=c)
         for pg in pgset:
-            componentProperty=ET.SubElement(componentProperties,'componentProperty',{'represented':'false'}) # [TBD3]
-            '''shortName'''
-            ET.SubElement(componentProperty,'shortName').text=pg.name
-            '''longName'''
-            ET.SubElement(componentProperty,'longName').text=pg.name
-
-            # the internal questionnaire representation is that all parameters
-            # are contained in a constraint group
             constraintSet=ConstraintGroup.objects.filter(parentGroup=pg)
-            for con in constraintSet:
-                pset=NewParam.objects.filter(constraint=con)
-                for p in pset:
-                    property=ET.SubElement(componentProperty,'componentProperty')
+            pset=NewParam.objects.filter(constraint=constraintSet[0])
+            if len(pset)>0:
+                componentProperty={}
+                if pg.name=="Component Attributes":
+                    componentProperty=componentProperties
+                else:
+                    componentProperty=ET.SubElement(componentProperties,'componentProperty',{'represented':str(c.implemented).lower()})
                     '''shortName'''
-                    ET.SubElement(property,'shortName').text=p.name
-                    '''longName'''
-                    ET.SubElement(property,'longName').text=p.name
-                    '''description'''
-                    '''type'''
-                    ET.SubElement(property,'type',{"value":"other"}) # TBD4
-                    '''value'''
-                    ET.SubElement(property,'value').text=p.value
+                ET.SubElement(componentProperty,'shortName').text=pg.name
+                '''longName'''
+                ET.SubElement(componentProperty,'longName').text=pg.name
+                
+                # the internal questionnaire representation is that all parameters
+                # are contained in a constraint group
+                for con in constraintSet:
+                    pset=NewParam.objects.filter(constraint=con)
+                    for p in pset:
+                        property=ET.SubElement(componentProperty,'componentProperty',{'represented':str(c.implemented).lower()})
+                        '''shortName'''
+                        ET.SubElement(property,'shortName').text=p.name
+                        '''longName'''
+                        ET.SubElement(property,'longName').text=p.name
+                        '''description'''
+                        '''value'''
+                    # extract all value elements (separated by "|"
+                        for value in p.value.split('|'):
+                            stripSpaceValue=value.strip()
+                            if stripSpaceValue!='':
+                                ET.SubElement(property,'value').text=stripSpaceValue
                     #ET.SubElement(property,'ptype').text=p.ptype
                     #ET.SubElement(property,'vocab').text=p.vocab
         '''numericalProperties'''
@@ -193,69 +318,62 @@ class NumericalModel:
         ET.SubElement(comp,'scientificProperties')
         '''grid'''
         '''responsibleParty'''
-        resp=ET.SubElement(comp,'responsibleParty') #type gmd:xxxx
-        ciresp=ET.SubElement(resp,self.GMD_NAMESPACE_BRACKETS+'CI_ResponsibleParty')
-        #http://www.isotc211.org/2005/gmd
-        #CI_ResponsibleParty referenced in citation.xsd
-        # <gmd:individualName>
-        #name=ET.SubElement(resp,'gmd:individualName')
-        #ET.SubElement(name,'gco:CharacterString').text=c.contact
-        name=ET.SubElement(ciresp,self.GMD_NAMESPACE_BRACKETS+'individualName')
-        ###ET.SubElement(name,self.GCO_NAMESPACE_BRACKETS+'CharacterString').text=c.contact
-        # </gmd:individualName/>
-        # <gmd:organisationName/>
-        # <gmd:positionName/>
-        # <gmd:contactInfo>
-        #contact=ET.SubElement(ciresp,'gmd:contactInfo')
-        contact=ET.SubElement(ciresp,self.GMD_NAMESPACE_BRACKETS+'contactInfo')
-        cicontact=ET.SubElement(contact,self.GMD_NAMESPACE_BRACKETS+'CI_Contact')
-        #     <gmd:phone/>
-        #     <gmd:address>
-        #address=ET.SubElement(cicontact,'gmd:address')
-        address=ET.SubElement(cicontact,self.GMD_NAMESPACE_BRACKETS+'address')
-        ciaddress=ET.SubElement(address,self.GMD_NAMESPACE_BRACKETS+'CI_Address')
-        #         <gmd:deliveryPoint/>
-        #         <gmd:city/>
-        #         <gmd:administrativeArea/>
-        #         <gmd:postalCode/>
-        #         <gmd:country/>
-        #         <gmd:electronicMailAddress>
-        #email=ET.SubElement(ciaddress,'gmd:electronicMailAddress')
-        #ET.SubElement(email,'gco:CharacterString').text=c.email
-        email=ET.SubElement(ciaddress,self.GMD_NAMESPACE_BRACKETS+'electronicMailAddress')
-        ###ET.SubElement(email,self.GCO_NAMESPACE_BRACKETS+'CharacterString').text=c.email
-        #         </gmd:electronicMailAddress>
-        #     </gmd:address>
-        #     <gmd:onlineResource/>
-        #     <gmd:hoursOfService/>
-        #     <gmd:contactInstructions/>
-        # </gmd:contactInfo>
-        # <gmd:roll/>
-        role=ET.SubElement(ciresp,self.GMD_NAMESPACE_BRACKETS+'role')#empty
+        comp.append(ET.Comment("format for responsible party's is not yet determined"))
+        resps=ET.SubElement(comp,"Q_responsibleParties")
+
+        self.addResp(resps,'author',c.author)
+        self.addResp(resps,'funder',c.funder)
+        self.addResp(resps,'contact',c.contact)
+        '''fundingSource'''
+        '''citation'''
+        references=ET.SubElement(comp,'Q_references')
+        for ref in c.references.all():
+            reference=ET.SubElement(references,'Q_reference')
+            ET.SubElement(reference,'Q_name').text=ref.name
+            ET.SubElement(reference,'Q_citation').text=ref.citation
+            ET.SubElement(reference,'Q_link').text=ref.link
+
+        ''' RF associating genealogy with the component rather than the document '''
+        if (nest<=2):
+            genealogy=ET.SubElement(comp,'Q_genealogy')
+            ET.SubElement(genealogy,'Q_yearReleased').text=str(c.yearReleased)
+            ET.SubElement(genealogy,'Q_previousVersion').text=c.otherVersion
+            ET.SubElement(genealogy,'Q_previousVersionImprovements').text=c.geneology
+
 
         '''activity'''
         '''type'''
-        comp.append(ET.Comment("value attribute in element type should have value "+c.scienceType+" but this fails the cim validation at the moment"))
-        ET.SubElement(comp,'type',{'value':'other'}) # c.scienceType
-        #ET.SubElement(comp,'type').text=c.scienceType
-        '''timing'''
-        comp.append(ET.Comment("Made up values for timing in order pass cim validation"))
-        timing=ET.SubElement(comp,'timing',{'units':'seconds'})
-        ET.SubElement(timing,'rate').text='1'
+        #comp.append(ET.Comment("value attribute in element type should have value "+c.scienceType+" but this fails the cim validation at the moment"))
+        #ET.SubElement(comp,'type',{'value':'other'}) # c.scienceType
+        ET.SubElement(comp,'type').text=c.scienceType
+        if nest==2:
+            '''timestep info only supplied for realm components'''
+            tsParam=self.getTimeStepParam(c)
+            if tsParam:
+                timing=ET.SubElement(comp,'timing',{'units':tsParam.units})
+                ET.SubElement(timing,'rate').text=str(tsParam.value)
+            else:
+                timing=ET.SubElement(comp,'timing',{'units':'not found'})
+                ET.SubElement(timing,'rate').text='not found'
         if nest==1:
-          '''documentID'''
-          ET.SubElement(comp,'documentID').text='[TBD6]'
-          '''documentAuthor'''
-          #ET.SubElement(comp,'documentAuthor').text=c.contact
-          ET.SubElement(comp,'documentAuthor')
-          '''documentCreationDate'''
-          #ET.SubElement(comp,'documentCreationDate').text=str(datetime.date.today())
-          ET.SubElement(comp,'documentCreationDate').text=str(datetime.date.today())+'T00:00:00'
-        '''documentGenealogy'''
-        '''quality'''
+            '''documentID'''
+            ET.SubElement(comp,'documentID').text=c.uri
+            '''documentAuthor'''
+            #ET.SubElement(comp,'documentAuthor').text=c.contact
+            ET.SubElement(comp,'documentAuthor').text="Metafor Questionnaire"
+            '''documentCreationDate'''
+            #ET.SubElement(comp,'documentCreationDate').text=str(datetime.date.today())
+            ET.SubElement(comp,'documentCreationDate').text=str(datetime.date.today())+'T00:00:00'
+            '''documentGenealogy'''
+            # component genealogy is provided in the questionnaire
+            '''quality'''
+            # no quality information in the questionnaire at the moment
+          
+
       else:
         logging.debug("component "+c.abbrev+" has implemented set to false")
       return
+
 
 class XMLVocabReader:
     # original author, Matt Pritchard
