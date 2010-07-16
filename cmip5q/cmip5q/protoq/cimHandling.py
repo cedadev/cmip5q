61afbec7bd0ff38fef94c73de9a4a0ff13fe23eb
paul <paul@4926d154-5f4e-0410-ae62-53d2c911a6ca>
2010-07-16 14:45:22 +0000

    Updated validation routine; __validationStep now uses applyXSLT()
    git-svn-id: http://proj.badc.rl.ac.uk/svn/metafor/cmip5q/trunk@1818 4926d154-5f4e-0410-ae62-53d2c911a6ca

--- cmip5q/cmip5q/protoq/cimHandling.py
+++ cmip5q/cmip5q/protoq/cimHandling.py
@@ -1,6 +1,9 @@
 import os,tempfile
 from lxml import etree as ET
 from django.conf import settings
+from Metafor import XMLreport
+import sys
+import StringIO
 logging=settings.LOG
 import pkg_resources
 
@@ -56,34 +59,60 @@ class Validator:
         self.report={}
 
     def errorsAsHtml(self) :
-        return self.report
+        #return self.report
+        HTMLresponse = ""
+
+        if self.nInvalid > 0:
+            HTMLresponse += "<h3>Status: <font class='red'>INVALID</font></h3>    {0} error{1} from {2} check{3}:<br/>".format(self.nInvalid, \
+                         "s" if (self.nInvalid != 1) else "", self.nChecks, \
+                         "s" if (self.nChecks != 1) else "" )
+
+            i = 1
+            for err in self.errlist:
+                HTMLresponse += "[{0}] {1}<br/>".format(i, err.text)
+                i += 1
+
+        else:
+           HTMLresponse += "<h3>Status: <font class='green'>VALID</font></h3> ({0} error{1} from {2} check{3})<br/>".format(self.nInvalid, \
+                         "s" if (self.nInvalid != 1) else "", self.nChecks, \
+                         "s" if (self.nChecks != 1) else "" )
+
+
+        return HTMLresponse
+        
     
     def xmlAsHtml(self) :
         return self.cimHtml
-    
+
     
     def __validationStep(self,CIMdoc,specificSchematronFile=None):
         ''' Common validation steps '''
         
         # perform any common validation
-        report,nChecks,nInvalid=self.__SchematronValidate(CIMdoc,"CommonChecks.sch")
-        self.report=report
-        self.nChecks=nChecks
-        self.nInvalid=nInvalid
+        #report,nChecks,nInvalid=self.__SchematronValidate(CIMdoc,"CommonChecks.sch")
+        checks, invalids, errorlist = self.applyXSLT(CIMdoc, "CommonChecks.sch")
+        #checks, invalids, errorlist = self.applyXSLT(CIMdoc, "quest.ruleset.sch")
+        #self.report=report
+        self.errlist = errorlist
+        self.nChecks = checks
+        self.nInvalid = invalids
     
+        #print "CIMdoc: {0}        self.report: {1}".format(type(CIMdoc), type(self.report) )
+
         if specificSchematronFile:
             # perform any specific validations
-            report,nChecks,nInvalid=self.__SchematronValidate(CIMdoc,specificSchematronFile)
+            #report,nChecks,nInvalid=self.__SchematronValidate(CIMdoc,specificSchematronFile)
+            checks, invalids, errorlist=self.applyXSLT(CIMdoc,specificSchematronFile)
             
             # add these results to the existing results
-            reportRoot=self.report.getroot()
-            reportRoot.append(report.getroot())
-            self.nChecks+=nChecks
-            self.nInvalid+=nInvalid
+            #reportRoot=self.report.getroot()
+            #reportRoot.append(report.getroot())
+            self.errlist += errorlist
+            self.nChecks += checks
+            self.nInvalid += invalids
         
         # calculate any derived state
         self.__updateState()
-    
 
     def __updateState(self):
 
@@ -109,6 +138,7 @@ class Validator:
         cimHtml = transform(formattedCIMdoc)
         return cimHtml
 
+    """
     def __SchematronValidate(self,CIMdoc,SchematronFile,SchematronReport="schematron-report.xsl"):
         ''' Validation common to both Component and Simulation '''
         sct_doc = ET.parse(os.path.join(self.XSLdir,SchematronFile))
@@ -122,6 +152,7 @@ class Validator:
         nChecks = len(report.xpath('//check'))
         nInvalid = len(report.xpath('//invalid'))
         return report,nChecks,nInvalid
+    """
 
     def validateFile(self,fileName):
         tmpDoc=ET.parse(file)
@@ -155,3 +186,90 @@ class Validator:
         # create an html representation of our document
         self.cimHtml=''#self.__CimAsHtml(CIMdoc)
         # don't really want to see the HTML for now ...
+
+
+    def __markup_rules_tree(self, stTree):
+        stRoot = stTree.getroot()
+        stxmlns  = stRoot.nsmap[None]
+        stxmlns_string = "{" + "{0}".format(stxmlns) + "}"
+
+        stRule = None
+        for elem in stRoot.iter():
+            #print "{0}elem[0] (len: {1}): {2}".format(type(elem), len(elem), elem.tag)
+            if len(elem) == 0:
+                continue
+            if type(elem) == ET._Comment:
+                continue
+
+            if elem.tag.endswith("rule") and len(elem) > 0:
+                    stRule = elem
+                    context = stRule.attrib["context"]
+
+                    repFlag = ET.Element(stxmlns_string + "report", nsmap=stRoot.nsmap)
+                    repFlag.attrib["test"] = "true()"
+                    #repFlag.text = "{0}: {1}".format(rule_count_flag, context)
+                    repFlag.text = "{0}:".format(rule_count_flag)
+
+                    stRule.append(repFlag)
+
+
+    def __markup_line_attr(self, stTree):
+
+        for elem in stTree.iter():
+            if type(elem) == ET._Comment:
+                continue
+            if elem.tag.endswith("report") or elem.tag.endswith("assert"):
+                lineelem = ET.fromstring('<value-of select="@__src_line_number_"/>')
+                lineelem.tail = ":" + elem.text
+                elem.text = "Line "
+                elem.insert(0, lineelem)
+
+
+    def applyXSLT(self, CIMdoc, rulesFile, xsltFile="schematron-report.xsl"):
+
+        try:
+            rulesTree = ET.parse(os.path.join(self.XSLdir, rulesFile))
+            xsltSource = open(os.path.join(self.XSLdir, xsltFile), 'r').read()
+            xsltTree = ET.parse(StringIO.StringIO(xsltSource))
+        except IOError as e:
+            print "IO error: Unable to read XSLT source file {0}".format(xsltFile)
+            sys.exit(1)
+        except ET.XMLSyntaxError as e:
+            print "XML Syntax error: Unable to parse source file {0}".format(xsltFile)
+            sys.exit(1)
+
+        i=1
+        for elem in CIMdoc.iter():
+            #elem.attrib["__src_line_number_"] = str(elem.sourceline)
+            elem.attrib["__src_line_number_"] = str(i)
+            i += 1
+
+        #self.__markup_line_attr(rulesTree)
+
+        xslt = ET.XSLT(xsltTree)
+        #self.__dumpTree()
+        xslt_schematron = xslt(rulesTree)
+        schematron = ET.XSLT(xslt_schematron)
+
+        #print "-=========: CIMdoc :==========-"
+        #print ET.tostring(CIMdoc)
+        #print "-===============================-"
+        #print "-=====: Transformed Output :=====-"
+        #print ET.tostring(xslt_schematron, pretty_print=True)
+        #print "=================================="
+
+        xml_output = schematron(CIMdoc)
+
+        #print "-=====: XML-Schematron Output :=====-"
+        #print ET.tostring(xml_output, pretty_print=True)
+        #print "-===================================-"
+
+        count = ET.XPath("count(//*[name() = $arg])")
+        checks = int(count(xml_output, arg="check"))
+        invalids = int(count(xml_output, arg="invalid"))
+
+        list_elements = ET.XPath("//*[name() = $arg]")
+        error_list = list_elements(xml_output, arg="li")
+
+        print "Checks: {0}  Invalids: {1}".format(checks, invalids)
+        return (checks, invalids, error_list)
