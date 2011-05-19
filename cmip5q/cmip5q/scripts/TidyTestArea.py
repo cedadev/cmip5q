41c7986e4b9617154b3fd4369d049f2677a85272
gerry <gerry@4926d154-5f4e-0410-ae62-53d2c911a6ca>
2011-05-19 13:19:50 +0000

    Parameter and constraint values now copied across
    git-svn-id: http://proj.badc.rl.ac.uk/svn/metafor/cmip5q/trunk@3058 4926d154-5f4e-0410-ae62-53d2c911a6ca

--- cmip5q/cmip5q/scripts/TidyTestArea.py
+++ cmip5q/cmip5q/scripts/TidyTestArea.py
@@ -7,12 +7,12 @@
     1. Delete (i.e. set to isDeleted) all grids, platforms, models, simulations 
        from both centres (**May change this to actual physical delete)
     2. Physically delete refs, files, parties
-    2. Copy HadGEM2-ES model from met office to example centre
+    3. Copy piControl from met office to example centre
     
     TO USE: from cmip5q directory issue:
-       > ./py scripts/Tidy_example.py (./py is the local badc directory python)
+       > ./py scripts/TidyTestArea.py (./py is the local badc directory python)
      
-    Author: Gerard Devine, University of Reading
+    Author: Gerard Devine, NCAS-CMS, University of Reading
     Date: 12/05/2011
 """
 
@@ -24,25 +24,253 @@ sys.path.insert(0, os.path.expanduser('..\protoq'))
 sys.path.insert(1, os.path.expanduser('..\..\cmip5q'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-#from django.conf import settings #@UnresolvedImport
-#logging=settings.LOG
-
 from cmip5q.protoq.models import *
 
 
+#*******************************************************************************
 # Settings used within this code
 testCentres = ['1. Example', '2. Test Centre']
 origCentre = Centre.objects.get(abbrev='MOHC')
 targetCentre = Centre.objects.get(abbrev='1. Example')
-origPlat = 'IBM Power 6'
 origModel = 'HadGEM2-ES'
 origSim = 'piControl'
+#*******************************************************************************
+
+
+def copyRPToNewCen(sourceRP):
+    """
+    Copy a responsible party from one centre to another centre (targetcentre)
+    """
+    logging.debug('***** Copying Responsible Party *****') 
+    
+    # Check that the RP doesn't already exist, in which case do nothing     
+    rps = ResponsibleParty.objects.filter(
+            centre=targetCentre, abbrev=sourceRP.abbrev)
+    if not rps:
+        rp = ResponsibleParty(isOrganisation = sourceRP.isOrganisation,
+                              name = sourceRP.name,
+                              webpage = sourceRP.webpage,
+                              abbrev = sourceRP.abbrev,
+                              email = sourceRP.email,
+                              address = sourceRP.address,
+                              uri = atomuri(),
+                              centre = targetCentre,
+                              )
+        rp.save()
+    
+        logging.debug('***** Completed responsible party copying *****')
+    
+        return rp
+    
+    else:
+        logging.debug('***** Responsible party already existed*****')
+        return rps[0]
+    
+
+def copyPlatToNewCen(sourcePlat):
+    """
+    Copy a platform from one centre to another centre
+    """
+    logging.debug('***** Copying Platform *****')
+    
+    # Retrieve and copy platform responsible parties
+    platContact = copyRPToNewCen(sourcePlat.contact) 
+    
+    p = Platform(abbrev = 'Example Platform',
+                 title = 'Example centre platform',
+                 centre = targetCentre,   
+                 contact = platContact,                      
+                 compiler = sourcePlat.compiler,
+                 compilerVersion = sourcePlat.compilerVersion,
+                 vendor = sourcePlat.vendor,
+                 maxProcessors = sourcePlat.maxProcessors,
+                 coresPerProcessor = sourcePlat.coresPerProcessor,
+                 operatingSystem = sourcePlat.operatingSystem,
+                 processor = sourcePlat.processor,
+                 hardware = sourcePlat.hardware, 
+                 interconnect = sourcePlat.interconnect,
+                 isDeleted = False,
+                 uri = atomuri())
+    p.save()
+    
+    logging.debug('***** Completed Platform copying *****')
+    
+    return p
+
+
+def copyRefToNewCen(sourceRef):
+    """
+    Copy a reference from one centre to another centre (targetcentre)
+    """
+    logging.debug('***** Copying Reference *****') 
+    
+    # Check that the RP doesn't already exist, in which case do nothing     
+    refs = Reference.objects.filter(
+            centre=targetCentre, name=sourceRef.name)
+    if not refs:
+        ref = Reference(name = sourceRef.name,
+                        citation = sourceRef.citation,
+                        link = sourceRef.link,
+                        refType = sourceRef.refType,
+                        centre = targetCentre,
+                        )
+        ref.save()
+    
+        logging.debug('***** Completed Reference copying *****')
+    
+        return ref
+    
+    else:
+        logging.debug('***** Reference already existed*****')
+        return refs[0]
+    
+    
+def copyPIToNewCen(sourcePI):
+    """
+    Copy parameter group information to the new model component 
+    """
+    logging.debug('***** Copying parameter information *****') 
+    
+    pgroup = ParamGroup(name=sourcePI.name)
+    pgroup.save()
+    
+    logging.debug('***** Completed parameter information *****') 
+    
+    
+def copyCompToNewCen(sourceComp, model=None, realm=None):
+    """
+    Copy a model from one centre to another centre
+    """ 
+    logging.debug('***** Copying Model *****')
+    
+    # Retrieve and copy platform responsible parties
+    if sourceComp.author:
+        compAuthor = copyRPToNewCen(sourceComp.author)
+    else:
+        compAuthor = None
+    if sourceComp.funder:
+        compFunder = copyRPToNewCen(sourceComp.funder)
+    else:
+        compFunder = None
+    if sourceComp.contact:
+        compContact = copyRPToNewCen(sourceComp.contact)
+    else:
+        compContact = None      
+    
+    m = Component(
+              abbrev = 'Example Model',
+              title = 'Example centre model (Based on MOHC HADGEM2-ES)',
+              centre = targetCentre,
+              documentVersion = sourceComp.documentVersion,
+              description = sourceComp.description,
+              validErrors = sourceComp.validErrors,
+              numberOfValidationChecks = sourceComp.numberOfValidationChecks,
+              isComplete = sourceComp.isComplete,
+              created = sourceComp.created,
+              updated = sourceComp.updated,
+              scienceType = sourceComp.scienceType,
+              implemented = sourceComp.implemented,
+              visited = sourceComp.visited,
+              controlled = sourceComp.controlled,
+              isRealm = sourceComp.isRealm,
+              isModel = sourceComp.isModel,
+              isParamGroup = sourceComp.isParamGroup,
+              yearReleased = sourceComp.yearReleased,
+              otherVersion = sourceComp.otherVersion,
+              isDeleted = sourceComp.isDeleted,
+              author = compAuthor,
+              funder = compFunder,
+              contact = compContact, 
+              uri = atomuri()
+              )    
+    m.save() 
+    
+    # now handle the associated references
+    for r in sourceComp.references.all().order_by('id'):
+        ref = copyRefToNewCen(r)
+        m.references.add(ref)
+    
+    m.save()
+    
+    # now sort out the links to parent model/realm    
+    if model is None:
+        if sourceComp.isModel:
+            model = m
+        else:
+            raise ValueError('Deep copy called with invalid model arguments')
+    elif realm is None:
+        if sourceComp.isRealm:
+            realm = m
+        else:
+            raise ValueError('Deep copy called with invalid realm arguments')
+    m.model = model
+    m.realm = realm
+    
+    m.save()
+    
+    # copy across all child component information
+    for c in sourceComp.components.all().order_by('id'):
+        cc = copyCompToNewCen(c, model=m.model, realm=m.realm)
+        
+        m.components.add(cc)
+    
+    # copy across all parameter information
+    for p in sourceComp.paramGroup.all().order_by('id'): 
+        m.paramGroup.add(p.copy())
+        
+    # and deal with the component inputs too ..
+    inputset = ComponentInput.objects.filter(owner=sourceComp).order_by('id')
+    for i in inputset: 
+        i.makeNewCopy(m)
+    m.save()
+    
+    logging.debug('***** Completed Model copying *****')
+    
+    return m
+
+
+def copyToNewCen():
+    """
+    Copy a full simulation from one centre to the targetCentre
+    """
+    logging.debug('***** Copying Simulation *****')
+    
+    # 1. Get the simulation to copy (with origCentre and origSim meaning only 
+    #    one can be returned)
+    sourceSim = Simulation.objects.filter(centre=origCentre, 
+                                          isDeleted=False).get(abbrev=origSim)
+    
+    # 2. Retrieve the platform used in this simulation and copy it
+    sourcePlat = sourceSim.platform
+    newPlat = copyPlatToNewCen(sourcePlat)
+            
+    # 3. Retrieve the model used in this simulation and copy it 
+    sourceComp = sourceSim.numericalModel
+    newMod = copyCompToNewCen(sourceComp)
+              
+    s = Simulation(abbrev ='Example_Sim', 
+                 title ='Example Simulation', 
+                 contact = sourceSim.contact, 
+                 author = sourceSim.author, 
+                 funder = sourceSim.funder,
+                 description = sourceSim.description, 
+                 authorList = sourceSim.authorList,
+                 uri = atomuri(),
+                 experiment = sourceSim.experiment, 
+                 numericalModel = newMod,
+                 ensembleMembers = sourceSim.ensembleMembers, 
+                 platform = newPlat,
+                 drsMember = sourceSim.drsMember, 
+                 centre = targetCentre)
+    s.save()
 
 
 def delElem(elem):
     """
     Sets a passed element, e.g. component/grid to be marked as isDeleted 
     """
+    assert hasattr(elem, 'isDeleted'), ("{0} is not of a type that can be "
+                                        "marked isDeleted").format(elem)
     elem.isDeleted = True
     elem.save()
 
@@ -63,60 +291,17 @@ def clearCentre(cent):
         block.objects.filter(centre=cent).delete()
 
 
-def copyPlatToExCen(sourcePlat):
-    """
-    Copy a platform from one centre to the example centre
-    """
-    logging.debug('***** Copying Platform *****')    
-    
-    p = Platform(abbrev = 'Example Platform',
-                 title = 'example centre platform',
-                 centre = targetCentre,                              
-                 compiler = sourcePlat.compiler,
-                 compilerVersion = sourcePlat.compilerVersion,
-                 vendor = sourcePlat.vendor,
-                 maxProcessors = sourcePlat.maxProcessors,
-                 coresPerProcessor = sourcePlat.coresPerProcessor,
-                 operatingSystem = sourcePlat.operatingSystem,
-                 processor = sourcePlat.processor,
-                 interconnect = sourcePlat.interconnect,
-                 isDeleted = False,
-                 uri = atomuri())
-    p.save()
-    
-    logging.debug('***** Completed Platform copying *****')
-    
-    
-def copyModToExCen(sourceModel):
-    """
-    Copy a model from one centre to the example centre
-    """ 
-    logging.debug('Copying Model')    
-    
-    source = Component.objects.filter(centre=origCentre).get(abbrev=sourceModel)
-    source.copy(targetCentre)
-    
-    logging.debug('***** Completed Model copying *****')
-
-
-def copySimToExCen(sourcePlat, sourceModel, sourceSim):
-    """
-    Copy a simulation from one centre to the example centre
-    """ 
-    s = Simulation(abbrev ='Example_Sim', 
-                 title ='Example Simulation', 
-                 contact = sourceSim.contact, 
-                 author = sourceSim.author, 
-                 funder = sourceSim.funder,
-                 description = sourceSim.description, 
-                 authorList = sourceSim.authorList,
-                 uri = atomuri(),
-                 experiment = sourceSim.experiment, 
-                 numericalModel = sourceSim.numericalModel,
-                 ensembleMembers = 1, 
-                 platform = sourceSim.platform, 
-                 centre = targetCentre)
-    s.save()    
+def emptyTestArea():
+    '''
+    Delete all entered information in the given testCentres
+    '''
+    logging.debug('***** START: Beginning deletion of test area centre '
+                   'information *****')
+    for cenabbrev in testCentres:
+        cent = Centre.objects.get(abbrev=cenabbrev)
+        clearCentre(cent)
+    logging.debug('***** END: Completed deletion of test area centre '
+                   'information *****')
 
 
 def tidyTestArea():
@@ -126,26 +311,10 @@ def tidyTestArea():
     2. copying updated information to example centre
     """
     logging.debug('***** START: Beginning tidy-up of test area centres *****')
-    
-    # 1. Delete Example and Test centre information
-    for cenabbrev in testCentres:
-        cent = Centre.objects.get(abbrev=cenabbrev)
-        clearCentre(cent)
-
-    # 2. Make a copy of official example (Mark Elkington's at this point)
-    # TODO: (GD_20110513) Check for uniqueness on the following to be copied
-    sourcePlat = Platform.objects.filter(centre=origCentre).get(abbrev=origPlat)
-    sourceModel = Component.objects.filter(centre=origCentre).get(abbrev=origModel)
-    sourceSim = Simulation.objects.filter(centre=origCentre).get(abbrev=origSim)
-    
-    copyPlatToExCen(sourcePlat)
-    copyModToExCen(sourceModel)
-    
-    #Now get newly copied model
-    exModel = Component.objects.filter(centre=targetCentre, isModel=True, 
-                                       isDeleted=False).get(abbrev=origModel+' dup')
-    copySimToExCen(sourcePlat, exModel, sourceSim)
-    
+        
+    emptyTestArea()
+    copyToNewCen()
+            
     logging.debug('***** END: Completed tidy-up of test area centres *****')
         
            
