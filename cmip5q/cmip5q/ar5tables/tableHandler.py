0a50dc77e3e42ea76a6fbc447f0a9251c665b3c0
gerry <gerry@4926d154-5f4e-0410-ae62-53d2c911a6ca>
2011-11-14 16:54:50 +0000

    Updated the csv output from ar5 table 1
    git-svn-id: http://proj.badc.rl.ac.uk/svn/metafor/cmip5q/trunk@3403 4926d154-5f4e-0410-ae62-53d2c911a6ca

--- cmip5q/cmip5q/ar5tables/tableHandler.py
+++ cmip5q/cmip5q/ar5tables/tableHandler.py
@@ -27,96 +27,133 @@ def ar5table1(models):
         
         #Check that realm is implemented
         m.aerimplemented = get_realmimpl(m, 'Aerosols')
-        #Get the abbrev
-        m.aerabbrev = get_realmabbrev(m, 'Aerosols')
-        #Get the component references
-        m.aerrefs, m.aercits = get_Refs(m, 'Aerosols')                
+        
+        if not m.aerimplemented:
+            m.aerabbrev, m.aerrefs, m.aercits = 'Not Implemented', 'Not Implemented', 'Not Implemented' 
+        else:
+            #Get the abbrev
+            m.aerabbrev = get_realmabbrev(m, 'Aerosols')
+            #Get the component references
+            m.aerrefs, m.aercits = get_Refs(m, 'Aerosols')                
         
         
         # 2. Get atmosphere column information
         
         #Check that realm is implemented
         m.atmosimplemented = get_realmimpl(m, 'Atmosphere')
-        #Get the abbrev
-        m.atmosabbrev = get_realmabbrev(m, 'Atmosphere')
-        #Get the component references
-        m.atmosrefs, m.atmoscits = get_Refs(m, 'Atmosphere')
-        #Get vert grid info
-        m.atmosgridtop, m.atmosnumlevels = get_vertgridinfo(m, 'Atmosphere')
-        #Get horizontal grid menmonic and resolution
-        atmosgridres, atmosgridmnem = get_HorGridRes(m, 'Atmosphere', 
-                                                     mnemonic=True)
-        m.atmoshorgrid = atmosgridmnem+' '+ atmosgridres
+        if not m.atmosimplemented:
+            m.atmosabbrev, 
+            m.atmosrefs, 
+            m.atmoscits,
+            m.atmosgridtop, 
+            m.atmosnumlevels,
+            m.atmoshorgrid =  'Not Implemented', 'Not Implemented', 'Not Implemented', 'Not Implemented', 'Not Implemented', 'Not Implemented'
+        else:
+            #Get the abbrev
+            m.atmosabbrev = get_realmabbrev(m, 'Atmosphere')
+            #Get the component references
+            m.atmosrefs, m.atmoscits = get_Refs(m, 'Atmosphere')
+            #Get vert grid info
+            m.atmosgridtop, m.atmosnumlevels = get_vertgridinfo(m, 'Atmosphere')
+            #Get horizontal grid menmonic and resolution
+            atmosgridres, atmosgridmnem = get_HorGridRes(m, 'Atmosphere', 
+                                                         mnemonic=True)
+            m.atmoshorgrid = atmosgridmnem+' '+ atmosgridres
                 
         
         # 3. Get atmospheric chemistry column information
         
         #Check that realm is implemented
         m.atmchemimplemented = get_realmimpl(m, 'AtmosphericChemistry')
-        #Get the abbrev
-        m.atmchemabbrev = get_realmabbrev(m, 'AtmosphericChemistry')
-        #Get the component references
-        m.atmchemrefs, m.atmoschemcits = get_Refs(m, 'AtmosphericChemistry')
+        if not m.atmchemimplemented:
+            m.atmchemabbrev, m.atmchemrefs, m.atmchemcits = 'Not Implemented', 'Not Implemented', 'Not Implemented' 
+        else:
+            #Get the abbrev
+            m.atmchemabbrev = get_realmabbrev(m, 'AtmosphericChemistry')
+            #Get the component references
+            m.atmchemrefs, m.atmchemcits = get_Refs(m, 'AtmosphericChemistry')
         
         
         # 4. Get land ice column information
         
         #Check that realm is implemented
         m.liceimplemented = get_realmimpl(m, 'LandIce')
-        #Get the abbrev
-        m.liceabbrev = get_realmabbrev(m, 'LandIce')
-        #Get the component references
-        m.licerefs, m.licecits = get_Refs(m, 'LandIce')
+        if not m.liceimplemented:
+            m.liceabbrev, m.licerefs, m.licecits = 'Not Implemented', 'Not Implemented', 'Not Implemented' 
+        else:
+            #Get the abbrev
+            m.liceabbrev = get_realmabbrev(m, 'LandIce')
+            #Get the component references
+            m.licerefs, m.licecits = get_Refs(m, 'LandIce')
         
         
         # 5. Get land surface column information
         
         #Check that realm is implemented
         m.lsurfimplemented = get_realmimpl(m, 'LandSurface')
-        #Get the abbrev
-        m.lsurfabbrev = get_realmabbrev(m, 'LandSurface')
-        #Get the component references
-        m.lsurfrefs, m.lsurfcits = get_Refs(m, 'LandSurface')
+        if not m.lsurfimplemented:
+            m.lsurfabbrev, m.lsurfrefs, m.lsurfcits = 'Not Implemented', 'Not Implemented', 'Not Implemented' 
+        else:
+            #Get the abbrev
+            m.lsurfabbrev = get_realmabbrev(m, 'LandSurface')
+            #Get the component references
+            m.lsurfrefs, m.lsurfcits = get_Refs(m, 'LandSurface')
         
         
         # 6. Get Ocean Biogeo column information
         
         #Check that realm is implemented
         m.obgcimplemented = get_realmimpl(m, 'OceanBiogeoChemistry')
-        #Get the abbrev
-        m.obgcabbrev = get_realmabbrev(m, 'OceanBiogeoChemistry')
-        #Get the component references
-        m.obgcrefs, m.obgccits = get_Refs(m, 'OceanBiogeoChemistry')
+        if not m.obgcimplemented:
+            m.obgcabbrev, m.obgcrefs, m.obgccits = 'Not Implemented', 'Not Implemented', 'Not Implemented' 
+        else:
+            #Get the abbrev
+            m.obgcabbrev = get_realmabbrev(m, 'OceanBiogeoChemistry')
+            #Get the component references
+            m.obgcrefs, m.obgccits = get_Refs(m, 'OceanBiogeoChemistry')
         
         
         # 7. Get Ocean information
         
         #Check that realm is implemented
         m.oceanimplemented = get_realmimpl(m, 'Ocean')
-        #Get the abbrev
-        m.oceanabbrev = get_realmabbrev(m, 'Ocean')
-        #Get the component references
-        m.oceanrefs, m.oceancits = get_Refs(m, 'Ocean')
-        #Get vert grid info
-        m.oceantoplevel, m.oceannumlevels = get_vertgridinfo(m, 'Ocean')
-        #Get the ocean grid z co-ordinate
-        m.zcoord = get_ZCoord(m, 'Ocean')
-        #Get the ocean top BC
-        m.oceantopbc = get_oceanTopBC(m)
-        #Get horizontal grid menmonic and resolution
-        oceangridres, oceangridmnem = get_HorGridRes(m, 'Ocean', 
-                                                     mnemonic=True)
-        m.oceanhorgrid = oceangridmnem+' '+ oceangridres
+        if not m.oceanimplemented:
+            m.oceanabbrev = 'Not Implemented' 
+            m.oceanrefs = 'Not Implemented'
+            m.oceancits = 'Not Implemented'
+            m.oceanhorgrid = 'Not Implemented'
+            m.oceannumlevels = 'Not Implemented'
+            m.oceanzcoord = 'Not Implemented'
+            m.oceantoplevel = 'Not Implemented'
+            m.oceantopbc = 'Not Implemented'
+        else:
+            #Get the abbrev
+            m.oceanabbrev = get_realmabbrev(m, 'Ocean')
+            #Get the component references
+            m.oceanrefs, m.oceancits = get_Refs(m, 'Ocean')
+            #Get vert grid info
+            m.oceantoplevel, m.oceannumlevels = get_vertgridinfo(m, 'Ocean')
+            #Get the ocean grid z co-ordinate
+            m.oceanzcoord = get_ZCoord(m, 'Ocean')
+            #Get the ocean top BC
+            m.oceantopbc = get_oceanTopBC(m)
+            #Get horizontal grid menmonic and resolution
+            oceangridres, oceangridmnem = get_HorGridRes(m, 'Ocean', 
+                                                         mnemonic=True)
+            m.oceanhorgrid = oceangridmnem+' '+ oceangridres
         
         
         # 8. Get Sea Ice column information
         
         #Check that realm is implemented
         m.seaiceimplemented = get_realmimpl(m, 'SeaIce')
-        #Get the abbrev
-        m.seaiceabbrev = get_realmabbrev(m, 'SeaIce')
-        #Get the component references
-        m.seaicerefs, m.seaicecits = get_Refs(m, 'SeaIce')
+        if not m.seaiceimplemented:
+            m.seaiceabbrev, m.seaicerefs, m.seaicecits = 'Not Implemented', 'Not Implemented', 'Not Implemented' 
+        else:
+            #Get the abbrev
+            m.seaiceabbrev = get_realmabbrev(m, 'SeaIce')
+            #Get the component references
+            m.seaicerefs, m.seaicecits = get_Refs(m, 'SeaIce')
         
         '''
         
@@ -144,8 +181,7 @@ def ar5table1(models):
         m.lsrefs, m.lscits = get_Refs(m, 'LandSurface')
         #Get information on river routing
         m.riverrouting, m.rrurl = get_lsrivrout(m)
-        
-        '''
+                '''
         
         
     return models
--- cmip5q/cmip5q/ar5tables/views.py
+++ cmip5q/cmip5q/ar5tables/views.py
@@ -50,54 +50,170 @@ def ar5csv(request):
     #write column headings
     writer.writerow(['Model ID', 
                      'Vintage', 
-                     'Institution', 
-                     'Atmosphere,Top', 
-                     'Hor. Resolution',
-                     'Atmos Refs',
-                     'Ocean Resolution',
-                     'Ocean Z Coordinate',
-                     'Ocean Top BC',
-                     'Ocean Refs',
-                     'Sea Ice Rheology',
-                     'Water Ponds?',
-                     'Lateral Melting?',
-                     'Sea Ice Refs',
-                     'River Routing?',
-                     'Land Surface Refs',
+                     'Institution',
+                     'Main references',
+                     'Flux correction',
+                     
+                     'Aerosol component name',
+                     'Aerosol code independance',
+                     'Aerosol references',
+                     
+                     'Atmosphere component name',
+                     'Atmosphere horizontal grid',
+                     'Atmosphere grid number of levels ',
+                     'Atmosphere grid top',
+                     'Atmosphere code independance',
+                     'Atmosphere references',
+                     
+                     'Atmos chemistry component name',
+                     'Atmos chemistry code independance',
+                     'Atmos chemistry references',
+                     
+                     'land ice component name',
+                     'Land ice code independance',
+                     'Land ice references',
+                     
+                     'Land surface component name',
+                     'Land surface code independance',
+                     'Land surface references',
+                     
+                     'Ocean biogeochem component name',
+                     'Ocean biogeochem code independance',
+                     'Ocean biogeochem references',
+                     
+                     'Ocean component name',
+                     'Ocean horizontal grid',
+                     'Ocean number of levels',
+                     'Ocean top level',
+                     'Ocean Z coordinate',
+                     'Ocean top BC',
+                     'Ocean code independance',
+                     'Ocean references',
+                     
+                     'Sea ice component name',
+                     'Sea ice code independance',
+                     'Sea ice references',
+                     
                      ])
     
     #write out each row of information in turn
     for row in table1info: 
         # first group references into a combined string
-        atmoscits=[]
-        for cit in row.atmoscits:
-            atmoscits.append(cit+'; ')
-        oceancits=[]
-        for cit in row.oceancits:
-            oceancits.append(cit+'; ')
-        seaicecits=[]
-        for cit in row.seaicecits:
-            seaicecits.append(cit+'; ')
-        lscits=[]
-        for cit in row.lscits:
-            lscits.append(cit+'; ')
+        maincits=[]
+        for cit in row.maincits:
+            maincits.append(cit+'; ')
+        maincits = "".join(maincits)
+        
+        if not row.aerimplemented:
+            aercits = 'None'
+        else:
+            aercits=[]
+            for cit in row.aercits:
+                aercits.append(cit+'; ')
+            aercits = "".join(aercits)
+        
+        if not row.atmosimplemented:
+            atmoscits = 'None'
+        else:
+            atmoscits=[]
+            for cit in row.atmoscits:
+                atmoscits.append(cit+'; ')
+            atmoscits = "".join(atmoscits)
+        
+        if not row.atmchemimplemented:
+            atmchemcits = 'None'
+        else:        
+            atmchemcits=[]
+            for cit in row.atmchemcits:
+                atmchemcits.append(cit+'; ')
+            atmchemcits = "".join(atmchemcits)
+        
+        if not row.liceimplemented:
+            licecits = 'None'
+        else:                
+            licecits=[]
+            for cit in row.licecits:
+                licecits.append(cit+'; ')
+            licecits = "".join(licecits)
+        
+        if not row.lsurfimplemented:
+            lsurfcits = 'None'
+        else:    
+            lsurfcits=[]
+            for cit in row.lsurfcits:
+                lsurfcits.append(cit+'; ')
+            lsurfcits = "".join(lsurfcits)
+        
+        if not row.obgcimplemented:
+            obgccits = 'None'
+        else:        
+            obgccits=[]
+            for cit in row.obgccits:
+                obgccits.append(cit+'; ')
+            obgccits = "".join(obgccits)
+        
+        if not row.oceanimplemented:
+            oceancits = 'None'
+        else:        
+            oceancits=[]
+            for cit in row.oceancits:
+                oceancits.append(cit+'; ')
+            oceancits = "".join(oceancits)
+                
+        if not row.seaiceimplemented:
+            seaicecits = 'None'
+        else:        
+            seaicecits=[]
+            for cit in row.seaicecits:
+                seaicecits.append(cit+'; ')
+            seaicecits = "".join(seaicecits)
+            
             
         writer.writerow([row.abbrev, 
                          row.yearReleased, 
-                         row.centre.name, 
-                         row.atmosgridtop, 
+                         row.centre.name,
+                         "".join(maincits),
+                         "Flux correction field",
+                         
+                         row.aerabbrev,
+                         "XX%",
+                         aercits,
+                         
+                         row.atmosabbrev,
                          row.atmoshorgrid,
+                         row.atmosnumlevels,
+                         row.atmosgridtop,
+                         "XX%",
                          "".join(atmoscits),
-                         row.oceangrid,
-                         row.zcoord,
-                         row.topbc,
+                         
+                         row.atmchemabbrev,
+                         "XX%",
+                         "".join(atmchemcits),
+                         
+                         row.liceabbrev,
+                         "XX%",
+                         "".join(licecits),
+                         
+                         row.lsurfabbrev,
+                         "XX%",
+                         "".join(lsurfcits),
+                         
+                         row.obgcabbrev,
+                         "XX%",
+                         "".join(obgccits),
+                         
+                         row.oceanabbrev,
+                         row.oceanhorgrid,
+                         row.oceannumlevels,
+                         row.oceantoplevel,
+                         row.oceanzcoord,
+                         row.oceantopbc,                         
+                         "XX%",
                          "".join(oceancits),
-                         row.sirheol,
-                         row.siwaterpond,
-                         row.silatmelt,
+                         
+                         row.seaiceabbrev,
+                         "XX%",
                          "".join(seaicecits),
-                         row.riverrouting,
-                         "".join(lscits),
                          ])
     
     return response
