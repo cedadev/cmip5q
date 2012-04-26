8deeff22cdf6d2ca44c48153555bfc29a3f218f4
gerry <gerry@4926d154-5f4e-0410-ae62-53d2c911a6ca>
2012-04-26 13:11:34 +0000

    removed erroneous directory
    git-svn-id: http://proj.badc.rl.ac.uk/svn/metafor/cmip5q/trunk@3588 4926d154-5f4e-0410-ae62-53d2c911a6ca

--- cmip5qn/cmip5q/protoq/admin/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-# Enabling the admin:
-from django.contrib import admin
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    (r'^admin/protoq/component/copy/$', 'cmip5q.protoq.admin.admin_views.modelcopy'),
-)
--- cmip5qn/cmip5q/scripts/README.txt
+++
@@ -1,2 +0,0 @@
-# This folder is used to hold general scripts used to do miscellaneous jobs 
-# with the questionnaire data
